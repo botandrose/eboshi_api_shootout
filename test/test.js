49f4b443e8b0719442e5161ee4c0e1ad2c775bc5
Micah Geisel <micah@botandrose.com>
2015-09-29 10:43:27 -0700

    get something super ugly working with list clients test.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var supertest = require('supertest');
+var mysql     = require('mysql');
 
 var request = function (path) {
     return supertest('http://localhost:6969')
@@ -6,8 +7,89 @@ var request = function (path) {
     .expect(200);
 }
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+});
+connection.connect();
+
 describe('/api/test', function() {
     it('returns "Hello world"', function (done) {
         request('/api/test').expect('Hello world', done);
     });
 });
+
+describe('/api/clients', function() {
+    before(function(done) {
+        connection.query("CREATE DATABASE IF NOT EXISTS `eboshi_test`;", function() {
+            connection.database = 'eboshi_test';
+            done();
+        });
+    });
+
+    before(function(done) {
+        connection.query("DROP TABLE IF EXISTS `clients`;", function() { done(); });
+    });
+
+    before(function(done) {
+        connection.query(
+            "CREATE TABLE `clients` (" +
+            "  `id` int(11) NOT NULL AUTO_INCREMENT," +
+            "  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL," +
+            "  `address` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL," +
+            "  `city` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL," +
+            "  `state` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL," +
+            "  `zip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL," +
+            "  `country` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL," +
+            "  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL," +
+            "  `contact` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL," +
+            "  `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL," +
+            "  `created_at` datetime DEFAULT NULL," +
+            "  `updated_at` datetime DEFAULT NULL," +
+            "  PRIMARY KEY (`id`)" +
+            ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;",
+            function() { done(); });
+    });
+
+    before(function(done) {
+        connection.query(
+            "INSERT INTO clients SET " +
+            "  name='Bot and Rose Design', " +
+            "  address='625 NW Everett St', " +
+            "  city='Portland', " +
+            "  state='OR', " +
+            "  zip='97209', " +
+            "  country='USA', " +
+            "  email='info@botandrose.com', " +
+            "  contact='Michael Gubitosa', " +
+            "  phone='(503) 662-2712', " +
+            "  created_at='2006-06-25T14:08:31Z', " +
+            "  updated_at='2015-08-29T09:58:23Z';",
+            function(err) { if(err) throw err; done(); }
+        );
+    });
+
+    it('returns a json list of clients', function (done) {
+        request('/api/clients').expect({
+            "data": [{
+                "type": "clients",
+                "id": "1",
+                "attributes": {
+                    "name": "Bot and Rose Design",
+                    "address": "625 NW Everett St",
+                    "city": "Portland",
+                    "state": "OR",
+                    "zip": "97209",
+                    "country": "USA",
+                    "email": "info@botandrose.com",
+                    "contact": "Michael Gubitosa",
+                    "phone": "(503) 662-2712",
+                    "created_at": "2006-06-25T14:08:31Z",
+                    "updated_at": "2015-08-29T09:58:23Z",
+                },
+            }]
+        }, done);
+    });
+});
+
