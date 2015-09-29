cd292f7f771bd7f3a2c058ee89481f8f0c67229f
Micah Geisel <micah@botandrose.com>
2015-09-29 09:41:49 -0700

    extract request test helper function.

--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
-var request = require('supertest');
+var supertest = require('supertest');
+
+var request = function (path) {
+    return supertest('http://localhost:6969')
+    .get(path)
+    .expect(200);
+}
 
 describe('/api/test', function() {
-
-    it('should return Hello world', function (done) {
-        request('http://localhost:6969')
-        .get('/api/test')
-        .expect(200)
-        .expect('Hello world', done);
+    it('returns "Hello world"', function (done) {
+        request('/api/test').expect('Hello world', done);
     });
-
 });
