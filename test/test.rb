9bcabd20175494f57a58c07d9ab38a58c2976c1f
Micah Geisel <micah@botandrose.com>
2015-10-01 16:13:42 -0700

    test for adding a client.

--- test/test.rb
+++ test/test.rb
@@ -50,4 +50,50 @@ describe "api" do
       })
     end
   end
+
+  describe "POST /api/clients" do
+    before do
+      skip_if_impl_in %w(elixir_phoenix node_express node_hapi ruby_sinatra)
+    end
+
+    it "persists the supplied json list of clients, and returns the result" do
+      response = post("/api/clients", {
+        "data" => [{
+          "type" => "clients",
+          "attributes" => {
+            "name" => "Bot and Rose Design",
+            "address" => "625 NW Everett St",
+            "city" => "Portland",
+            "state" => "OR",
+            "zip" => "97209",
+            "country" => "USA",
+            "email" => "info@botandrose.com",
+            "contact" => "Michael Gubitosa",
+            "phone" => "(503) 662-2712",
+            "created_at" => "2006-06-25T14:08:31Z",
+            "updated_at" => "2015-08-29T09:58:23Z",
+          },
+        }]
+      })
+      response.must_equal({
+        "data" => [{
+          "type" => "clients",
+          "id" => "1",
+          "attributes" => {
+            "name" => "Bot and Rose Design",
+            "address" => "625 NW Everett St",
+            "city" => "Portland",
+            "state" => "OR",
+            "zip" => "97209",
+            "country" => "USA",
+            "email" => "info@botandrose.com",
+            "contact" => "Michael Gubitosa",
+            "phone" => "(503) 662-2712",
+            "created_at" => "2006-06-25T14:08:31Z",
+            "updated_at" => "2015-08-29T09:58:23Z",
+          },
+        }]
+      })
+    end
+  end
 end
