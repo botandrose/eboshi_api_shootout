377192fa4201a7cac14f7db9f9c80d32ab1217f6
Micah Geisel <micah@botandrose.com>
2015-10-01 17:13:35 -0700

    extract http test helper and refactor.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,5 @@
-require "net/http"
-require "json"
 require_relative "./db"
+require_relative "./http"
 
 def db
   $db ||= DB.new.tap(&:bootstrap)
@@ -11,21 +10,9 @@ def skip_if_impl_in impls
 end
 
 def get url
-  request :get, url
+  Request.get(url).body
 end
 
 def post url, data
-  request :post, url, data
+  Request.post(url, data).body
 end
-
-def request method, url, data=nil
-  response = Net::HTTP.new("localhost", 6969).start do |http|
-    request = Net::HTTP.const_get(method.to_s.capitalize).new(url, initheader = {'Content-Type' =>'application/json'})
-    request.body = JSON.dump(data) if data
-    http.request(request)
-  end
-  JSON.load(response.body)
-rescue JSON::ParserError
-  response.body
-end
-
