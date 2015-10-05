f9ecc74271a94a8038833629fdad51c41f4c17ff
Micah Geisel <micah@botandrose.com>
2015-10-04 23:42:01 -0700

    collapse response wrapper type into serializer.

--- haskell_scotty/Server.hs
+++ haskell_scotty/Server.hs
@@ -11,33 +11,23 @@ main = scotty 6969 $ do
     html "Hello world"
 
   get "/api/clients" $ do
-    Web.Scotty.json ClientResponseWrapper {
-      dada=[
-        ClientResponse {
-          tipe="clients",
-          Main.id="1",
-          attributes=ClientAttributes {
-            name="Bot and Rose Design",
-            address="625 NW Everett St",
-            city="Portland",
-            state="OR",
-            Main.zip="97209",
-            country="USA",
-            email="info@botandrose.com",
-            contact="Michael Gubitosa",
-            phone="(503) 662-2712",
-            created_at="2006-06-25T14:08:31Z",
-            updated_at="2015-08-29T09:58:23Z"
-          }
-        }
-      ]
+    Web.Scotty.json ClientResponse {
+      tipe="clients",
+      Main.id="1",
+      attributes=ClientAttributes {
+        name="Bot and Rose Design",
+        address="625 NW Everett St",
+        city="Portland",
+        state="OR",
+        Main.zip="97209",
+        country="USA",
+        email="info@botandrose.com",
+        contact="Michael Gubitosa",
+        phone="(503) 662-2712",
+        created_at="2006-06-25T14:08:31Z",
+        updated_at="2015-08-29T09:58:23Z"
+      }
     }
-
-data ClientResponseWrapper = ClientResponseWrapper {
-  dada :: [ClientResponse]
-}
-instance ToJSON ClientResponseWrapper where
-  toJSON p = object [ "data" .= dada p ]
 
 data ClientResponse = ClientResponse {
   tipe :: String,
@@ -45,7 +35,11 @@ data ClientResponse = ClientResponse {
   attributes :: ClientAttributes
 }
 instance ToJSON ClientResponse where
-  toJSON p = object [ "type" .= tipe p, "id" .= Main.id p, "attributes" .= attributes p ]
+  toJSON a = object [
+    "data" .= [ object [
+      "type" .= tipe a,
+      "id" .= Main.id a,
+      "attributes" .= attributes a ] ] ]
 
 data ClientAttributes = ClientAttributes {
   name :: String,
