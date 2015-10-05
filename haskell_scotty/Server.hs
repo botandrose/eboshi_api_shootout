9c2a75d0708a9eb1cb399cc3f43edd8cacf2f8e1
Micah Geisel <micah@botandrose.com>
2015-10-04 23:55:13 -0700

    collapse all types into a single Client serializer.

--- haskell_scotty/Server.hs
+++ haskell_scotty/Server.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
 
 import Web.Scotty
 import Data.Aeson
-import GHC.Generics
 
 main :: IO ()
 main = scotty 6969 $ do
@@ -11,37 +9,23 @@ main = scotty 6969 $ do
     html "Hello world"
 
   get "/api/clients" $ do
-    Web.Scotty.json ClientResponse {
-      tipe="clients",
+    Web.Scotty.json Client {
       Main.id="1",
-      attributes=ClientAttributes {
-        name="Bot and Rose Design",
-        address="625 NW Everett St",
-        city="Portland",
-        state="OR",
-        Main.zip="97209",
-        country="USA",
-        email="info@botandrose.com",
-        contact="Michael Gubitosa",
-        phone="(503) 662-2712",
-        created_at="2006-06-25T14:08:31Z",
-        updated_at="2015-08-29T09:58:23Z"
-      }
+      name="Bot and Rose Design",
+      address="625 NW Everett St",
+      city="Portland",
+      state="OR",
+      Main.zip="97209",
+      country="USA",
+      email="info@botandrose.com",
+      contact="Michael Gubitosa",
+      phone="(503) 662-2712",
+      created_at="2006-06-25T14:08:31Z",
+      updated_at="2015-08-29T09:58:23Z"
     }
 
-data ClientResponse = ClientResponse {
-  tipe :: String,
+data Client = Client {
   id :: String,
-  attributes :: ClientAttributes
-}
-instance ToJSON ClientResponse where
-  toJSON a = object [
-    "data" .= [ object [
-      "type" .= tipe a,
-      "id" .= Main.id a,
-      "attributes" .= attributes a ] ] ]
-
-data ClientAttributes = ClientAttributes {
   name :: String,
   address :: String,
   city :: String,
@@ -53,6 +37,24 @@ data ClientAttributes = ClientAttributes {
   phone :: String,
   created_at :: String,
   updated_at :: String
-} deriving Generic
-instance ToJSON ClientAttributes
+}
+
+instance ToJSON Client where
+  toJSON client = object [
+    "data" .= [ object [
+      "type" .= ("clients" :: String),
+      "id" .= Main.id client,
+      "attributes" .= object [
+        "name" .= name client,
+        "address" .= address client,
+        "city" .= city client,
+        "state" .= state client,
+        "zip" .= Main.zip client,
+        "country" .= country client,
+        "email" .= email client,
+        "contact" .= contact client,
+        "phone" .= phone client,
+        "created_at" .= created_at client,
+        "updated_at" .= updated_at client ] ] ] ]
+
 
