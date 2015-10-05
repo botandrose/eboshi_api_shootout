c995f9be7882222e25a861abed6b561b83240f2f
Bart Massey <bart@cs.pdx.edu>
2015-10-05 01:52:16 -0700

    added a second endpoint, more logic, comments, types
    The server now has a second endpoint, which errs. The router
    logic has been expanded to accommodate this. There are
    comments.  Things have been moved to the top level and
    typed, for safety and clarity.

--- haskell_scotty/Server.hs
+++ haskell_scotty/Server.hs
@@ -1,7 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
+import Network.HTTP.Types.Status
 
-main = scotty 6969 $ do
-    get "/api/test" $ do
-        html "Hello world"
+-- | Build a basic test page.
+helloTestPage :: ActionM ()
+helloTestPage = html "Hello world"
+
+-- | Build a test page and do some status stuff.
+compoundTestPage :: ActionM ()
+compoundTestPage = do
+  html "You are not supposed to be here."
+  status badRequest400
+
+-- | Specify the behavior of each API endpoint.
+router :: ScottyM ()
+router = do
+  get "/api/test" helloTestPage
+  get "/api/test2" compoundTestPage
+
+-- | Construct the actual webserver.
+main :: IO ()
+main = scotty 6969 router
 
