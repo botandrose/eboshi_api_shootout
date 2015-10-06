d0838dbb9b412d454cbdae7917b53944efd54add
Bart Massey <bart@cs.pdx.edu>
2015-10-06 01:02:34 -0700

    got client working with null mysql query; cleaned a bunch of stuff up
    This change is fairly large.
    
    * Got the type right for fetchId.
    * Put the right liftIO in to get the Client record populated.
    * Cleaned up the cabal file dependencies again.
    * Added back some comments.

--- haskell_scotty/Server.hs
+++ haskell_scotty/Server.hs
@@ -1,34 +1,46 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Web.Scotty
 import Client
-import Database.MySQL.Simple
 import Control.Monad
+import Control.Monad.IO.Class
+import Data.Word
+import Database.MySQL.Simple
+import Web.Scotty
 
-fetchId :: IO String
+-- | Fetch a client ID from MySQL.
+fetchId :: IO Word64
 fetchId = do
-  conn <- connect defaultConnectInfo
-  results <- query_ conn "SELECT 1"
-  return $ head $ forM results $ \(id) -> (id)
+  conn <- connect defaultConnectInfo {
+            connectUser = "test",
+            connectPassword = "test" }
+  [Only result] <- query_ conn "SELECT 1"
+  return result
 
+-- | Generate a simple test page.
+testPage :: ActionM ()
+testPage = html "Hello world"
 
+-- | Respond to an API "clients" request with JSON.
+clientsPage :: ActionM ()
+clientsPage = do
+  clientId <- liftIO fetchId
+  json Client {
+    Client.id=show clientId,
+    name="Bot and Rose Design",
+    address="625 NW Everett St",
+    city="Portland",
+    state="OR",
+    Client.zip="97209",
+    country="USA",
+    email="info@botandrose.com",
+    contact="Michael Gubitosa",
+    phone="(503) 662-2712",
+    created_at="2006-06-25T14:08:31Z",
+    updated_at="2015-08-29T09:58:23Z"
+  }
+
+-- | Provide the top-level API.
 main :: IO ()
 main = scotty 6969 $ do
-  get "/api/test" $ do
-    html "Hello world"
-
-  get "/api/clients" $ do
-    json Client {
-      Client.id=fetchId,
-      name="Bot and Rose Design",
-      address="625 NW Everett St",
-      city="Portland",
-      state="OR",
-      Client.zip="97209",
-      country="USA",
-      email="info@botandrose.com",
-      contact="Michael Gubitosa",
-      phone="(503) 662-2712",
-      created_at="2006-06-25T14:08:31Z",
-      updated_at="2015-08-29T09:58:23Z"
-    }
+  get "/api/test" testPage
+  get "/api/clients" clientsPage
 
