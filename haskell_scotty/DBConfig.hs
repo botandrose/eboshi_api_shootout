108bf865b5bd37d547d9ae1e6567021ac03dea8e
Micah Geisel <micah@botandrose.com>
2015-10-06 13:57:02 -0700

    extract duplication.

--- haskell_scotty/DBConfig.hs
+++ haskell_scotty/DBConfig.hs
@@ -12,20 +12,16 @@ data DBConfig = DBConfig {
 
 readDBConfig :: IO DBConfig
 readDBConfig = do
-  userMaybe <- lookupEnv "EBOSHI_API_SHOOTOUT_MYSQL_USERNAME"
-  user <- case userMaybe of
-    Just string -> return string
-    Nothing -> return "root"
-
-  passwordMaybe <- lookupEnv "EBOSHI_API_SHOOTOUT_MYSQL_PASSWORD"
-  password <- case passwordMaybe of
-    Just string -> return string
-    Nothing -> return ""
-
-  databaseMaybe <- lookupEnv "EBOSHI_API_SHOOTOUT_MYSQL_DATABASE"
-  database <- case databaseMaybe of
-    Just string -> return string
-    Nothing -> return ""
-
+  user <- lookupEnvOrDefault "EBOSHI_API_SHOOTOUT_MYSQL_USERNAME" "root"
+  password <- lookupEnvOrDefault "EBOSHI_API_SHOOTOUT_MYSQL_PASSWORD" ""
+  database <- lookupEnvOrDefault "EBOSHI_API_SHOOTOUT_MYSQL_DATABASE" "eboshi_test"
   return $ DBConfig user password database
+
+lookupEnvOrDefault :: String -> String -> IO String
+lookupEnvOrDefault key defaultValue = do
+  valueMaybe <- lookupEnv key
+  value <- case valueMaybe of
+    Just string -> return string
+    Nothing -> return defaultValue
+  return value
 
