2c44db9a21ea42bc2a88b70a008017ca1a04b26b
Tian Song <tian.y.song@gmail.com>
2015-10-07 00:12:45 -0700

    massage tuple correctly.

--- python_flask/hello.py
+++ python_flask/hello.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.mysql import MySQL
 from flask import jsonify
+import dateutil.parser as parser
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -14,15 +15,34 @@ mysql.init_app(app)
 
 @app.route("/api/test")
 def hello():
-    return "Hello World"
+    return "Hello world"
 
 @app.route('/api/clients')
 def clients():
     cursor = mysql.connect().cursor()
     cursor.execute('''SELECT * FROM clients''')
-    clients = cursor.fetchall()
-    print clients
-    return jsonify(clients)
+    ltpv = [tpv for tpv in cursor]
+    lk = [ltpk[0] for ltpk in cursor.description]
+    laResults = []
+    for tpv in ltpv:
+        attributes = {}
+        for k, v in zip(lk, tpv):
+            if k != 'id':
+                attributes[k] = unicode(v)
+            else:
+                clientId = unicode(v)
+
+        attributes['created_at'] = parser.parse(attributes['created_at']).isoformat()
+        attributes['updated_at'] = parser.parse(attributes['updated_at']).isoformat()
+
+        clients = {
+            'type': 'clients',
+            'id': clientId,
+            'attributes': attributes
+        }
+
+        laResults.append(clients)
+    return jsonify({ 'data': laResults })
 
 if __name__ == "__main__":
     app.debug = True
