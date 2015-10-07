9251d163198edebf73397e6af0bf0a1e6d17d929
Micah Geisel <micah@botandrose.com>
2015-10-07 14:49:11 -0700

    extract FetchClients module.

--- haskell_scotty/Server.hs
+++ haskell_scotty/Server.hs
@@ -1,24 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Client
-import DBConfig
-import Control.Monad
-import Control.Monad.IO.Class
-import Database.MySQL.Simple
 import Web.Scotty
-
--- | Fetch a client from MySQL.
-fetchClients :: IO [Client]
-fetchClients = do
-  DBConfig user pass database <- readDBConfig
-  conn <- connect defaultConnectInfo {
-            connectUser = user,
-            connectPassword = pass,
-            connectDatabase = database }
-  results <- query_ conn "SELECT * FROM clients"
-  clients <- forM results $ \(id, name, address, city, state, zip, country, email, contact, phone, created_at, updated_at) ->
-    return $ Client id name address city state zip country email contact phone created_at updated_at
-  return clients
+import FetchClients
+import Control.Monad.IO.Class
 
 -- | Generate a simple test page.
 testPage :: ActionM ()
