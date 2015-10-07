656c7b74df59aa330eef02b7309057513d9673af
Micah Geisel <micah@botandrose.com>
2015-10-07 14:50:26 -0700

    reformat main scotty function to look more like sinatra's DSL.

--- haskell_scotty/Server.hs
+++ haskell_scotty/Server.hs
@@ -4,19 +4,12 @@ import Web.Scotty
 import FetchClients
 import Control.Monad.IO.Class
 
--- | Generate a simple test page.
-testPage :: ActionM ()
-testPage = html "Hello world"
-
--- | Respond to an API "clients" request with JSON.
-clientsPage :: ActionM ()
-clientsPage = do
-  clients <- liftIO fetchClients
-  json $ head clients
-
--- | Provide the top-level API.
 main :: IO ()
 main = scotty 6969 $ do
-  get "/api/test" testPage
-  get "/api/clients" clientsPage
+  get "/api/test" $ do
+    html "Hello world"
+
+  get "/api/clients" $ do
+    clients <- liftIO fetchClients
+    json $ head clients
 
