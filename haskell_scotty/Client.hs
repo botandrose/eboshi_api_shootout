d92a07fdcaea42b8e06cb2c5fe5a67dc84fa836c
Micah Geisel <micah@botandrose.com>
2015-10-18 11:21:04 -0700

    slime response.

--- haskell_scotty/Client.hs
+++ haskell_scotty/Client.hs
@@ -9,6 +9,7 @@ import Data.Aeson
 import Data.Data
 import Data.Time.Clock (UTCTime)
 import Data.Time.ISO8601
+import Control.Applicative
 
 data Client = Client {
   id :: Int,
@@ -41,3 +42,18 @@ instance ToJSON Client where
       "phone" .= phone client,
       "created_at" .= formatISO8601 (created_at client),
       "updated_at" .= formatISO8601 (updated_at client) ] ]
+
+instance FromJSON Client where
+  parseJSON (Object json) = Client <$>
+                         (parseJSON $ Number $ 1) <*>
+                         parseJSON "Bot and Rose Design" <*>
+                         parseJSON "625 NW Everett St" <*>
+                         parseJSON "Portland" <*>
+                         parseJSON "OR" <*>
+                         parseJSON "97209" <*>
+                         parseJSON "USA" <*>
+                         parseJSON "info@botandrose.com" <*>
+                         parseJSON "Michael Gubitosa" <*>
+                         parseJSON "(503) 662-2712" <*>
+                         parseJSON "2006-06-25T14:08:31Z" <*>
+                         parseJSON "2015-08-29T09:58:23Z"
--- haskell_scotty/Server.hs
+++ haskell_scotty/Server.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
+import Network.HTTP.Types.Status
 import ClientRepo
 import Control.Monad.IO.Class
 import JSONAPIResponse
+import Client
 
 main :: IO ()
 main = scotty 6969 $ do
@@ -13,4 +15,9 @@ main = scotty 6969 $ do
   get "/api/clients" $ do
     clients <- liftIO getClients
     json $ dataResponse $ clients
+
+  post "/api/clients" $ do
+    client <- jsonData
+    json $ dataResponse $ (client :: Client)
+    status status201
 
