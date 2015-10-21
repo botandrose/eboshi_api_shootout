3f4865582a2cfa64f025cf0af82ef2194299ce8c
Micah Geisel <micah@botandrose.com>
2015-10-20 20:59:45 -0700

    deslime INSERT.

--- haskell_scotty/ClientRepo.hs
+++ haskell_scotty/ClientRepo.hs
@@ -7,6 +7,8 @@ import ConstructResult
 import DBConnection
 import Database.MySQL.Simple
 import Database.MySQL.Simple.QueryResults
+import Database.MySQL.Simple.QueryParams
+import Database.MySQL.Simple.Param
 
 getClients :: IO [Client]
 getClients = do
@@ -14,23 +16,22 @@ getClients = do
   clients <- query_ conn "SELECT * FROM clients"
   return clients
 
+instance QueryResults Client where
+  convertResults fs vs = Client $... zip fs vs
+
 saveClient :: Client -> IO Client
 saveClient client = do
   conn <- connectDB
-  execute_ conn "INSERT INTO clients SET \
-    \name=\"Bot and Rose Design\", \
-    \address=\"625 NW Everett St\", \
-    \city=\"Portland\", \
-    \state=\"OR\", \
-    \zip=\"97209\", \
-    \country=\"USA\", \
-    \email=\"info@botandrose.com\", \
-    \contact=\"Michael Gubitosa\", \
-    \phone=\"(503) 662-2712\", \
-    \created_at=\"2006-06-25T14:08:31\", \
-    \updated_at=\"2015-08-29T09:58:23\";"
+  execute conn "INSERT INTO clients \
+    \(name,address,city,state,zip,country,email,contact,phone,created_at,updated_at) values \
+    \(?,?,?,?,?,?,?,?,?,?,?)" client
   return client { Client.id = (1 :: Int) }
 
 instance QueryResults Client where
   convertResults fs vs =
       Client $... constructorWrap (undefined :: Client) fs vs
+
+instance QueryParams Client where
+  renderParams
+    (Client _ name address city state zip country email contact phone created_at updated_at) =
+      [render name, render address, render city, render state, render zip, render country, render email, render contact, render phone, render created_at, render updated_at]
