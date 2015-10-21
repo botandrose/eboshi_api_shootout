96a1dd98dd98e26027ff7a365318230969c3389c
Micah Geisel <micah@botandrose.com>
2015-10-20 20:38:44 -0700

    actually insert the client, slimily.

--- haskell_scotty/ClientRepo.hs
+++ haskell_scotty/ClientRepo.hs
@@ -16,20 +16,19 @@ getClients = do
 
 saveClient :: Client -> IO Client
 saveClient client = do
-  -- conn <- connectDB
-  -- results <- query_ conn "INSERT INTO clients SET \
-  --         \name=\"Bot and Rose Design\", \
-  --         \address=\"625 NW Everett St\", \
-  --         \city=\"Portland\", \
-  --         \state=\"OR\", \
-  --         \zip=\"97209\", \
-  --         \country=\"USA\", \
-  --         \email=\"info@botandrose.com\", \
-  --         \contact=\"Michael Gubitosa\", \
-  --         \phone=\"(503) 662-2712\", \
-  --         \created_at=\"2006-06-25T14:08:31\", \
-  --         \updated_at=\"2015-08-29T09:58:23\";"
-  --
+  conn <- connectDB
+  execute_ conn "INSERT INTO clients SET \
+    \name=\"Bot and Rose Design\", \
+    \address=\"625 NW Everett St\", \
+    \city=\"Portland\", \
+    \state=\"OR\", \
+    \zip=\"97209\", \
+    \country=\"USA\", \
+    \email=\"info@botandrose.com\", \
+    \contact=\"Michael Gubitosa\", \
+    \phone=\"(503) 662-2712\", \
+    \created_at=\"2006-06-25T14:08:31\", \
+    \updated_at=\"2015-08-29T09:58:23\";"
   return client { Client.id = (1 :: Int) }
 
 instance QueryResults Client where
