599b60823169894156d8fe02f6db994c9edd2e17
Bart Massey <bart@cs.pdx.edu>
2015-10-22 00:41:57 -0700

    cleaned up DBConnection

--- haskell_scotty/DBConnection.hs
+++ haskell_scotty/DBConnection.hs
@@ -4,29 +4,18 @@ module DBConnection (connectDB) where
 import Database.MySQL.Simple
 import System.Environment
 
-data DBConfig = DBConfig {
-      dbConfigUser :: String,
-      dbConfigPassword :: String,
-      dbConfigDatabase :: String }
-
-readDBConfig :: IO DBConfig
-readDBConfig = do
-  user <- lookupEnvOrDefault "EBOSHI_API_SHOOTOUT_MYSQL_USERNAME" "root"
-  password <- lookupEnvOrDefault "EBOSHI_API_SHOOTOUT_MYSQL_PASSWORD" ""
-  database <- lookupEnvOrDefault "EBOSHI_API_SHOOTOUT_MYSQL_DATABASE" "eboshi_test"
-  return $ DBConfig user password database
-
 lookupEnvOrDefault :: String -> String -> IO String
 lookupEnvOrDefault key defaultValue = do
-  valueMaybe <- lookupEnv key
-  value <- case valueMaybe of
+  valueMaybe <- lookupEnv $ "EBOSHI_API_SHOOTOUT_MYSQL_" ++ key
+  case valueMaybe of
     Just string -> return string
     Nothing -> return defaultValue
-  return value
 
 connectDB :: IO Connection
 connectDB = do
-  DBConfig user pass database <- readDBConfig
+  user <- lookupEnvOrDefault "USERNAME" "root"
+  pass <- lookupEnvOrDefault "PASSWORD" ""
+  database <- lookupEnvOrDefault "DATABASE" "eboshi_test"
   conn <- Database.MySQL.Simple.connect defaultConnectInfo {
     connectUser = user,
     connectPassword = pass,
