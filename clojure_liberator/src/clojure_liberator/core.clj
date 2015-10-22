bf876745d03da67b297c5333be702b3e71e3c170
John Watson <jkwatson@gmail.com>
2015-10-21 21:40:05 -0700

    Implementation of the first clients api test.

--- clojure_liberator/src/clojure_liberator/core.clj
+++ clojure_liberator/src/clojure_liberator/core.clj
@@ -1,20 +1,43 @@
 (ns clojure-liberator.core
-  (:gen-class)
+  ;(:gen-class)
   (:require [liberator.core :refer [resource defresource]]
             [ring.middleware.params :refer [wrap-params]]
             [compojure.core :refer [defroutes ANY]]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [clojure-liberator.data-access :as data-access]
+            [clojure.data.json :as json]
+            [clj-time.format :as fmt]
+            [clj-time.core :as t]
+            clj-time.jdbc
+            )
+  (:import (org.joda.time DateTime)))
+
+(def formatter (fmt/formatters :date-time-no-ms))
+
+(extend-type DateTime
+  json/JSONWriter
+  (-write [date out]
+    (json/-write (fmt/unparse formatter date) out)))
+
+(defn transform-type [type-name value]
+  {:id (str (:id value)) :type type-name :attributes (dissoc value :id)})
+
+(def client-transform (partial transform-type "clients"))
 
 (defresource hello-world
              :available-media-types ["text/plain"]
              :handle-ok "Hello world")
 
+(defresource all-clients
+             :available-media-types ["application/json"]
+             :handle-ok {:data (map client-transform (data-access/all_clients))})
+
 (defroutes app
-           (ANY "/api/test" [] hello-world))
+           (ANY "/api/test" [] hello-world)
+           (ANY "/api/clients" [] all-clients))
 
 (def handler
   (-> app wrap-params))
 
 (defn -main []
-  (.addShutdownHook (Runtime/getRuntime) (Thread. #(println "shutdown")))
   (jetty/run-jetty handler {:port 6969 :join? true :daemon? true}))
---
+++ clojure_liberator/src/clojure_liberator/data_access.clj
@@ -0,0 +1,22 @@
+(ns clojure-liberator.data-access)
+(use 'korma.db)
+(use 'korma.core)
+(require '[clojure.string :as str])
+
+(defdb eboshi (mysql {:db "eboshi_test"
+                       :user "root"
+                       :password ""
+                       ;; optional keys
+                       }))
+
+(declare clients users)
+
+(defentity clients)
+
+(defentity users)
+
+(defn all_users []
+  (select users))
+
+(defn all_clients []
+  (select clients))
