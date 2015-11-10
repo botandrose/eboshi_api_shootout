61b849e4dcac04f9cc0a6a370d6258572f032b14
Micah Geisel <micah@botandrose.com>
2015-11-09 20:27:39 -0800

    extract DBModel abstraction.

--- crystal_kemal/src/client.cr
+++ crystal_kemal/src/client.cr
@@ -1,35 +1,7 @@
-require "mysql"
+require "./db_model"
 
-class Client
-  def self.connection
-    @@connection ||= MySQL.connect("127.0.0.1", "root", "", "eboshi_test", 3306_u16, nil)
-  end
-
-  def self.all
-    connection.query("SELECT * FROM #{@@table_name}").not_nil!.map do |row|
-      attributes = {} of Symbol => MySQL::Types::SqlType
-      row.not_nil!.size.times do |index|
-        attributes[@@fields[index]] = row[index]
-      end
-      new attributes
-    end
-  end
-
-  def initialize attributes
-    @attributes = attributes
-  end
-
+class Client < DBModel
   @@table_name = "clients"
-
-  @@fields = [] of Symbol
-
-  macro attribute(field, type)
-    @@fields.push :{{field}}
-
-    def {{field}}
-      @attributes[:{{field}}] as {{type}}
-    end
-  end
 
   attribute id, Int32
   attribute name, String
@@ -43,24 +15,5 @@ class Client
   attribute phone, String
   attribute created_at, Time
   attribute updated_at, Time
-
-  def as_json_api_hash
-    attributes = {} of Symbol => String
-    @@fields.each do |field|
-      next if field == :id
-      value = @attributes[field]
-      if value.is_a? Time
-        attributes[field] = value.to_s("%FT%TZ")
-      else
-        attributes[field] = value.to_s
-      end
-    end
-
-    {
-      type: @@table_name,
-      id: id.to_s,
-      attributes: attributes,
-    }
-  end
 end
 
---
+++ crystal_kemal/src/db_model.cr
@@ -0,0 +1,53 @@
+require "mysql"
+
+class DBModel
+  def self.connection
+    @@connection ||= MySQL.connect("127.0.0.1", "root", "", "eboshi_test", 3306_u16, nil)
+  end
+
+  def self.all
+    connection.query("SELECT * FROM #{@@table_name}").not_nil!.map do |row|
+      attributes = {} of Symbol => MySQL::Types::SqlType
+      row.not_nil!.size.times do |index|
+        attributes[fields[index]] = row[index]
+      end
+      new attributes
+    end
+  end
+
+  def initialize attributes
+    @attributes = attributes
+  end
+
+  def self.fields
+    @@fields ||= [] of Symbol
+  end
+
+  macro attribute(field, type)
+    fields.push :{{field}}
+
+    def {{field}}
+      @attributes[:{{field}}] as {{type}}
+    end
+  end
+
+  def as_json_api_hash
+    attributes = {} of Symbol => String
+    self.class.fields.each do |field|
+      next if field == :id
+      value = @attributes[field]
+      if value.is_a? Time
+        attributes[field] = value.to_s("%FT%TZ")
+      else
+        attributes[field] = value.to_s
+      end
+    end
+
+    {
+      type: @@table_name,
+      id: id.to_s,
+      attributes: attributes,
+    }
+  end
+end
+
