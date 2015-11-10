ee368242583804da70e62636fc29ec13a8c0a1bc
Micah Geisel <micah@botandrose.com>
2015-11-09 19:28:03 -0800

    pull the data from mysql, very very uglily.

--- crystal_kemal/src/client.cr
+++ crystal_kemal/src/client.cr
@@ -1,24 +1,27 @@
+require "mysql"
+
 class Client
   def self.all
-    [
+    conn = MySQL.connect("127.0.0.1", "root", "", "eboshi_test", 3306_u16, nil)
+    conn.query("SELECT * FROM clients").not_nil!.map do |row|
       {
-        :type=>"clients",
-        :id=>"1",
-        :attributes=>{
-          :name=>"Bot and Rose Design",
-          :address=>"625 NW Everett St",
-          :city=>"Portland",
-          :state=>"OR",
-          :zip=>"97209",
-          :country=>"USA",
-          :email=>"info@botandrose.com",
-          :contact=>"Michael Gubitosa",
-          :phone=>"(503) 662-2712",
-          :created_at=>"2006-06-25T14:08:31Z",
-          :updated_at=>"2015-08-29T09:58:23Z"
+        type: "clients",
+        id: row.shift.to_s,
+        attributes: {
+          name: row.shift.to_s,
+          address: row.shift.to_s,
+          city: row.shift.to_s,
+          state: row.shift.to_s,
+          zip: row.shift.to_s,
+          country: row.shift.to_s,
+          email: row.shift.to_s,
+          contact: row.shift.to_s,
+          phone: row.shift.to_s,
+          created_at: (row.shift as Time).to_s("%FT%TZ"),
+          updated_at: (row.shift as Time).to_s("%FT%TZ"),
         }
       }
-    ]
+    end
   end
 end
 
