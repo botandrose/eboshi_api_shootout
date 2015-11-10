2546fd923e32aeba92896405e2a81f6dd94f9980
Micah Geisel <micah@botandrose.com>
2015-11-09 19:51:27 -0800

    slightly more general.

--- crystal_kemal/src/client.cr
+++ crystal_kemal/src/client.cr
@@ -7,21 +7,26 @@ class Client
 
   def self.all
     connection.query("SELECT * FROM clients").not_nil!.map do |row|
+      fields = %i(id name address city state zip country email contact phone created_at updated_at)
+      attributes = {} of Symbol => (Nil | String | Int32 | Int64 | Float64 | Bool | Time | MySQL::Types::Date)
+      row.not_nil!.size.times do |index|
+        attributes[fields[index]] = row[index]
+      end
       {
         type: "clients",
-        id: row.shift.to_s,
+        id: attributes.delete(:id).to_s,
         attributes: {
-          name: row.shift.to_s,
-          address: row.shift.to_s,
-          city: row.shift.to_s,
-          state: row.shift.to_s,
-          zip: row.shift.to_s,
-          country: row.shift.to_s,
-          email: row.shift.to_s,
-          contact: row.shift.to_s,
-          phone: row.shift.to_s,
-          created_at: (row.shift as Time).to_s("%FT%TZ"),
-          updated_at: (row.shift as Time).to_s("%FT%TZ"),
+          name: attributes[:name].to_s,
+          address: attributes[:address].to_s,
+          city: attributes[:city].to_s,
+          state: attributes[:state].to_s,
+          zip: attributes[:zip].to_s,
+          country: attributes[:country].to_s,
+          email: attributes[:email].to_s,
+          contact: attributes[:contact].to_s,
+          phone: attributes[:phone].to_s,
+          created_at: (attributes[:created_at] as Time).to_s("%FT%TZ"),
+          updated_at: (attributes[:updated_at] as Time).to_s("%FT%TZ"),
         }
       }
     end
