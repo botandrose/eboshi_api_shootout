cff73984e9e9bc38d78c7ee75d9464786ce6d3a7
Micah Geisel <micah@botandrose.com>
2015-11-15 11:06:35 -0800

    add clock in test.

--- test/api/line_items_test.rb
+++ test/api/line_items_test.rb
@@ -52,5 +52,50 @@ describe "line items API" do
       })
     end
   end
+
+  describe "PATCH /api/clients/:client_id/clock_in" do
+    before do
+      skip_if_impl_in %w(clojure_liberator crystal_kemal elixir_phoenix haskell_scotty kotlin_spark node_express node_hapi python_flask ruby_sinatra)
+
+      @token = sign_up_and_authorize_account({
+        name: "Micah Geisel",
+        email: "micah@botandrose.com",
+        password: "omgponies",
+      })
+    end
+
+    it "creates a new line item" do
+      response = patch("/api/clients/1/clock_in", {
+        data: {
+          type: "line_items",
+          attributes: {
+            rate: 100.00,
+            notes: "Troubleshooting PEBKAC",
+          }
+        }
+      }, "Authorization" => "Bearer #{@token}")
+
+      response.json_body.must_equal_json({
+        data: {
+          type: "line_items",
+          id: "1",
+          attributes: {
+            client_id: "1",
+            user_id: "1",
+            start: ISO_8601_PATTERN,
+            finish: nil,
+            hours: nil,
+            rate: 100.00,
+            total: nil,
+            notes: "Troubleshooting PEBKAC",
+            created_at: ISO_8601_PATTERN,
+            updated_at: ISO_8601_PATTERN,
+            invoice_id: nil,
+            type: "LineItem",
+          }
+        }
+      })
+    end
+  end
 end
 
